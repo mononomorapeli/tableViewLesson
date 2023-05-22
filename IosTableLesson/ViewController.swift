683bad6c8ee2e8c041b3188d1816f94d663481fd
mononomorapeli <minkymorapeli@gmail.com>
2023-05-22 16:15:18 +0200

    Lesson learnt

--- IosTableLesson/ViewController.swift
+++ IosTableLesson/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var cars = ["VIVO","Audi","Mazda"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+extension ViewController : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cars.count
+        
+    }//reuse ever
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = cars[indexPath.row]
+        return cell
+    }
+    
 }
 
